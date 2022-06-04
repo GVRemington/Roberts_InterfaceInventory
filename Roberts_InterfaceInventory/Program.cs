5cf17893bbb82910ea3777c82f1cb5d675597e16
GVRemington <104083156+GVRemington@users.noreply.github.com>
2022-06-03 21:59:07 -0700

    fixed comments

--- Roberts_InterfaceInventory/Program.cs
+++ Roberts_InterfaceInventory/Program.cs
@@ -2,20 +2,16 @@
 
 //Rayleen Roberts
 //IT 112
-//Notes: Worked this while loop around a lot. I thought of one 
-       //more thing...splitting the two "returns" away from the ADD
-       //and having them be their own method.  But then I ran out of
-       //time to complete that thing. After I turn this in, I'm going 
-       //to fiddle with it and see if that works, but I won't update it
-       //on gitHub...I know that would be cheating. I'm also disappointed
-       //that I couldn't get rid of reading evaluating readkey's in main...or at 
-       //least I couldn't figure out how. 
-//Behaviors not implemented: none of them because I never could get my loop to do 
-                           //what I wanted...but I think if I could, populating the array
-                           //and evaluating it would have worked...except like I said above
-                           //I now think I needed to separate reading the array and adding 
-                           //to the array into two methods. Did the best I coudl with time
-                           //allowed and health issues going on.  Beter luck to me next time :)
+//Notes: Worked this while loop around a lot. I've been trying breakpoints.
+//       for whatever reason, this isn't letting me put breakpoints in the 
+//       class, (or maybe I just don't know where to put them in the class),
+//       I need to figure out if the information is going in and then I am erasing 
+//       it, or if it isn't getting there in the first place. I'll keep going. 
+//Behaviors not implemented: either the loop is not populating the array, or it is and 
+//                           then the index is getting erased/replaced. I can iterate
+//                           through the loops for the counts, and get the shipping cost
+//                           of the one item that will show in the array...but I still have 
+//                           and Array Population bug somewhere, still looking. 
 
 using Roberts_InterfaceInventory;
 
